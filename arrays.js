3c972bba490ba0b989d55f976eb375cbe7b2e9fc
VICTORIA STEPHANNE :) <84507283+magasteph@users.noreply.github.com>
2024-01-11 13:44:50 -0300

    Delete arrays.js

--- arrays.js
+++
@@ -1,8 +0,0 @@
-let cargos = ["administrador", "advogado", "médico", "analista de sistemas"];
-// 0,1,2,3
-cargos [2] = "contador";
-
-cargos.push("devops");
-cargos.pop("devops");
-
-console.log(cargos);
